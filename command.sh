644f24b4a88a5b4bfe6b3b8b1e101f7961eeec0c
adwin596@gmail.com <adwin596@gmail.com>
2017-08-13 01:56:23 -0700

    init upload

---
+++ command.sh
@@ -0,0 +1,10 @@
+#python flow --imgdir  sample_img/ --model cfg/yolo.cfg --load yolo.weights --gpu 0.8
+python flow --imgdir  ../adwin_NV_test/480_imgs/ --model cfg/yolo.cfg --load yolo.weights --gpu 0.8 --json
+
+
+
+
+
+
+
+
